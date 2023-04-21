6d7c65d0fb05f0e762357bb0f0ac28bd8d3a6d2a
bsv-khoavo <khoavo@bravesoft.com.vn>
2023-04-21 16:58:15 +0700

    Update

--- shophousefrontend/src/components/left/NotedListHouse.js
+++ shophousefrontend/src/components/left/NotedListHouse.js
@@ -1,26 +1,23 @@
 import React, { useState, useEffect } from 'react'
 import HouseCard from '../basecomponents/HouseCard'
 import apiWorker from '../../lib/apiFactory'
+import ReactDOM from "react-dom/client";
 
 
 const MAX_DISPLAY_NOTED_HOUSE = 5
 const MAX_STORED_NOTED_HOUSE = 20
 let gStartIndex = 0
 function NotedListHouse() {
-    // houseIndex is set default by useState(0 -> house index array start from 0)
-    // useState is not execute immediately
     const [houseList, setHouseList] = useState({});
-    // with handle response is not work at first its will wait send request to work
-    // -> send request is async so mean the code is not set up late
-    // but its still resposne is receive right? so why this not work
     apiWorker.SetAPIResource("localhost:6060", "/house/houses", "GET", {}, "", {})
     apiWorker.WithHandleResponse((data) => {
         console.log("Setting houseList state:", data)
         setHouseList(data)
     })
     useEffect(() => {
+        console.log("useEffect called")
         apiWorker.SendRequest()
-    }, [houseList])
+    }, [name])
     console.log("houseList state:" + houseList)// this is null
     return generateList(apiWorker.response)
 }
